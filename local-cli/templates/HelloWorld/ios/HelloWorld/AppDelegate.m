7a441f090401fe8cc6d7395466fbba745d44ddc8
tsochetra <than.sochetra@icloud.com>
2018-07-23 01:13:28 +0700

    modified ios folder google map api

--- local-cli/templates/HelloWorld/ios/HelloWorld/AppDelegate.m
+++ local-cli/templates/HelloWorld/ios/HelloWorld/AppDelegate.m
@@ -9,13 +9,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+@import GoogleMaps;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  [GMSServices provideAPIKey:@"API_KEY"];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
